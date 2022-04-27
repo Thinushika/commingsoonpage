43d9bfe38166bcc06377fc430be22d9a50857f79
PraveenMatheesha <77117024+Praveen-47@users.noreply.github.com>
2022-04-27 11:34:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import css from "./App.module.css"
+import bgImage from './assets/bg.png'
+import logo from './assets/logo.png'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee} from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFontAwesome,faReddit, faInstagram,faDiscord} from '@fortawesome/free-brands-svg-icons'
+
+import ReactPlayer from 'react-player/youtube'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className={css.container}>
+      <div
+       className={css.backgroundImage}
+       style={{
+        backgroundImage:`url(${bgImage})`,
+        position:"fixed",
+        backgroundSize:"cover",
+        width:"100%",
+        height:"464px",
+        backgroundSize:"cover",
+        // top:"10px"
+        // backgroundPosition: "top"
+      }}>
+    {/* <img src={bgImage}/> */}
+      <div className={css.NavBar}>
+        <div className={css.logoWrapper}>
+          <img src={logo} alt="Logo" className={css.logo} style={{ width: "150px" }} />
+        </div>
+        <div className={css.socialIconsWrapper}>
+          <FontAwesomeIcon icon={faTwitter} className={css.icon} />
+          <FontAwesomeIcon icon={faReddit} className={css.icon} />
+          <FontAwesomeIcon icon={faInstagram} className={css.icon} />
+          <FontAwesomeIcon icon={faDiscord} className={css.icon} />
+          <FontAwesomeIcon icon={faFontAwesome} className={css.icon} />
+        </div>
+      </div>
+      <div className={css.headerSection}>
+        <span className={css.styledHeaderText}>
+          COMMING SOON
+        </span>
+        <span className={css.headerTextTitle}>
+          THE CYBERHIPPOZ PROJECT
+        </span>
+        <span className={css.headerTextTitle}>
+          (HIP)
+        </span>
+        <span className={css.headerTextSubTitle}>
+          A collection of <span>10,000 highly advanced hippos </span>, from the <br />digital planet Potomia, living on the ethereum blockchain
+        </span>
+      </div>
+      <div className={css.aboutsection}>
+    <div className={css.aboutText}>
+      WELCOME TO POTOMIA.
     </div>
+    <div className={css.youtubeSection}>
+    {/* <div className={css.playerwrapper}> */}
+    <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' className={css.player}/>
+      {/* </div> */}
+    </div>
+    <div className={css.footerIcons}>
+      <FontAwesomeIcon icon={faTwitter} className={css.icon} />
+      <FontAwesomeIcon icon={faReddit} className={css.icon} />
+      <FontAwesomeIcon icon={faInstagram} className={css.icon} />
+      <FontAwesomeIcon icon={faDiscord} className={css.icon} />
+      <FontAwesomeIcon icon={faFontAwesome} className={css.icon} />
+    </div>
+ </div> 
+    </div>
+    {/* About section */}
+
+  
+  </div>
   );
 }
 
