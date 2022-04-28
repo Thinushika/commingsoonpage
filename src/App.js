fc81285f31dd6e2a6e578f3215bc152706affe2c
Thinushi <thinupavani@gmail.com>
2022-04-28 09:23:00 +0530

    video section responsive fixed

--- src/App.js
+++ src/App.js
@@ -1,85 +1,102 @@
-import React from "react"
-import css from "./App.module.css"
-import bgImage from './assets/bg.jpg'
-import logo from './assets/logo.png'
+import React from "react";
+import css from "./App.module.css";
+import bgImage from "./assets/bg.jpg";
+import logo from "./assets/logo.png";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee} from '@fortawesome/free-solid-svg-icons'
-import { faTwitter, faFontAwesome,faReddit, faInstagram,faDiscord} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter,
+  faFontAwesome,
+  faReddit,
+  faInstagram,
+  faDiscord,
+} from "@fortawesome/free-brands-svg-icons";
 
-import ReactPlayer from 'react-player/youtube'
+import ReactPlayer from "react-player/youtube";
 
 function App() {
   return (
-  <div className={css.container}>
+    <div className={css.container}>
       <div
-       className={css.backgroundImage}
-       style={{
-        backgroundImage:`url(${bgImage})`,
-        
-        backgroundSize:"cover",
-        width:"auto",
-        // height:"auto",
-        backgroundSize:"cover",
-        // top:"10px"
-        // backgroundPosition: "top"
-      }}>
-    {/* <img src={bgImage}/> */}
-      <div className={css.NavBar}>
-        <div className={css.logoWrapper}>
-          <img src={logo} alt="Logo" className={css.logo} style={{ width: "150px" }} />
+        className={css.backgroundImage}
+        style={{
+          backgroundImage: `url(${bgImage})`,
+
+          backgroundSize: "cover",
+          width: "auto",
+          // height:"auto",
+          backgroundSize: "cover",
+          // top:"10px"
+          // backgroundPosition: "top"
+        }}
+      >
+        {/* <img src={bgImage}/> */}
+        <div className={css.NavBar}>
+          <div className={css.logoWrapper}>
+            <img
+              src={logo}
+              alt="Logo"
+              className={css.logo}
+              style={{ width: "150px" }}
+            />
+          </div>
+          <div className={css.socialIconsWrapper}>
+            <FontAwesomeIcon icon={faTwitter} className={css.icon} />
+            <FontAwesomeIcon icon={faReddit} className={css.icon} />
+            <FontAwesomeIcon icon={faInstagram} className={css.icon} />
+            <FontAwesomeIcon icon={faDiscord} className={css.icon} />
+            <FontAwesomeIcon icon={faFontAwesome} className={css.icon} />
+          </div>
         </div>
-        <div className={css.socialIconsWrapper}>
-          <FontAwesomeIcon icon={faTwitter} className={css.icon} />
-          <FontAwesomeIcon icon={faReddit} className={css.icon} />
-          <FontAwesomeIcon icon={faInstagram} className={css.icon} />
-          <FontAwesomeIcon icon={faDiscord} className={css.icon} />
-          <FontAwesomeIcon icon={faFontAwesome} className={css.icon} />
+        <div className={css.headerSection}>
+          <span className={css.styledHeaderText}>COMMING SOON</span>
+          <span className={css.headerTextTitle}>THE CYBERHIPPOZ PROJECT</span>
+          <span className={css.headerTextTitle}>(HIP)</span>
+          <span className={css.headerTextSubTitle}>
+            A collection of <span>10,000 highly advanced hippos </span>, from
+            the <br />
+            digital planet Potomia, living on the ethereum blockchain
+          </span>
         </div>
-      </div>
-      <div className={css.headerSection}>
-        <span className={css.styledHeaderText}>
-          COMMING SOON
-        </span>
-        <span className={css.headerTextTitle}>
-          THE CYBERHIPPOZ PROJECT
-        </span>
-        <span className={css.headerTextTitle}>
-          (HIP)
-        </span>
-        <span className={css.headerTextSubTitle}>
-          A collection of <span>10,000 highly advanced hippos </span>, from the <br />digital planet Potomia, living on the ethereum blockchain
-        </span>
-      </div>
-      <div className={css.aboutsection}>
-    <div className={css.aboutText}>
-      WELCOME TO POTOMIA.
-    </div>
-    <div className={css.youtubeSection}>
-    <div>
-    {/* className='player-wrapper' */}
-        <ReactPlayer
+        <div className={css.aboutsection}>
+          <div className={css.aboutText}>WELCOME TO POTOMIA.</div>
+          {/* <div className={css.youtubeSection}>
+    <div> */}
+          {/* className='player-wrapper' */}
+          {/* <ReactPlayer
           className='react-player'
           url='https://www.youtube.com/watch?v=HRtC2sDiKqM'
           width='100%'
           height='100%'
         />
       </div>
-    </div>
-    <div className={css.footerIcons}>
-      <FontAwesomeIcon icon={faTwitter} className={css.icon} />
-      <FontAwesomeIcon icon={faReddit} className={css.icon} />
-      <FontAwesomeIcon icon={faInstagram} className={css.icon} />
-      <FontAwesomeIcon icon={faDiscord} className={css.icon} />
-      <FontAwesomeIcon icon={faFontAwesome} className={css.icon} />
-    </div>
-    <div className={css.empty}></div>
- </div> 
-    </div>
-    {/* About section */}
+    </div> */}
 
-  
-  </div>
+          <div className={css.youtubeSection}>
+            <div className={css.video}>
+              {/* className='player-wrapper' */}
+              <ReactPlayer
+                className="react-player"
+                url="https://www.youtube.com/watch?v=HRtC2sDiKqM"
+                width="100%"
+                height="100%"
+              />
+            </div>
+          </div>
+
+          <div className={css.footerIcons}>
+            <FontAwesomeIcon icon={faTwitter} className={css.icon} />
+            <FontAwesomeIcon icon={faReddit} className={css.icon} />
+            <FontAwesomeIcon icon={faInstagram} className={css.icon} />
+            <FontAwesomeIcon icon={faDiscord} className={css.icon} />
+            <FontAwesomeIcon icon={faFontAwesome} className={css.icon} />
+          </div>
+          <div className={css.empty}></div>
+        </div>
+      </div>
+      {/* About section */}
+    </div>
   );
 }
 
